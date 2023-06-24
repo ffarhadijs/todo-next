a5a78f746b1c0c9c532907e7356deed71d7e0c13
farhad <rb0214192@gmail.com>
2023-06-24 11:20:00 +0330

    add react query and mantine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,20 @@ import {
   ColorSchemeProvider,
   ColorScheme,
 } from "@mantine/core";
+import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import { Notifications } from "@mantine/notifications";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const router = useRouter();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  const [queryClient] = useState(() => new QueryClient());
 
+  const notFound = router.pathname === "/404";
+  const signin = router.pathname === "/signin";
+  const signup = router.pathname === "/signup";
   return (
     <ColorSchemeProvider
       colorScheme={colorScheme}
@@ -22,9 +30,18 @@ export default function App({ Component, pageProps }: AppProps) {
         withGlobalStyles
         withNormalizeCSS
       >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Notifications/>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            {notFound || signin || signup ? (
+              <Component {...pageProps} />
+            ) : (
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            )}
+          </Hydrate>
+        </QueryClientProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
