88d3c304020d11c0c30b29df1571a4d4ca9210dd
farhad <rb0214192@gmail.com>
2023-06-26 01:44:06 +0330

    create api for update profile

---
+++ pages/api/auth/updateProfile/index.ts
@@ -0,0 +1,53 @@
+import TodoUser from "@/models/TodoUser";
+import { connectDB } from "@/utils/connectDB";
+import verifyToken from "@/utils/verifyToken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  try {
+    await connectDB();
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Error on connecting to DB",
+    });
+  }
+
+  const { token } = req.cookies;
+  const secretKey = process.env.SECRET_KEY;
+
+  if (!token) {
+    res.status(401).json({
+      status: "Failed",
+      message: "You are not signed in",
+    });
+  }
+  const { email } = verifyToken(token!, secretKey!);
+
+  if (!email) {
+    return res
+      .status(401)
+      .json({ status: "failed", messgae: "you are unauthorized" });
+  }
+
+  const { firstName, lastName } = req.body;
+
+  const user = await TodoUser.findOne({ email });
+
+  user.firstName = firstName;
+  user.lastName = lastName;
+
+  await user.save();
+
+  res.status(201).json({
+    status: "Success",
+    message: "User data updated succesfully",
+  });
+}
