23ca1ea7152c888135aace33557e6162a60bb6af
farhad <rb0214192@gmail.com>
2023-06-26 01:46:11 +0330

    add loader

--- components/layout/LeftNavbar.tsx
+++ components/layout/LeftNavbar.tsx
@@ -1,3 +1,4 @@
+import { QueryKey } from "@/enums/queryKey.enum";
 import {
   Navbar,
   Text,
@@ -6,17 +7,29 @@ import {
   Divider,
   Box,
   UnstyledButton,
+  Loader,
+  Center,
 } from "@mantine/core";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 import { UserCircle, ArrowRight } from "tabler-icons-react";
 
 const LeftNavbar = ({ opened }: { opened: boolean }) => {
+  const { push } = useRouter();
   const theme = useMantineTheme();
+  const { data, isLoading } = useQuery({
+    queryKey: [QueryKey.GetUser],
+    queryFn: () => axios.get("/api/auth/getUser"),
+    refetchOnMount: "always",
+  });
+
   return (
     <Navbar
       p={"md"}
       hiddenBreakpoint="xs"
       hidden={!opened}
-      width={{ xs: 200, lg: 300 }}
+      width={{ xs: 200, lg: 250 }}
       style={{
         width: opened ? "200px" : "0",
       }}
@@ -25,6 +38,7 @@ const LeftNavbar = ({ opened }: { opened: boolean }) => {
       <Divider my="md" />
       <Navbar.Section>
         <UnstyledButton
+          onClick={() => push("/profile")}
           w="100%"
           p="sm"
           sx={{
@@ -36,23 +50,30 @@ const LeftNavbar = ({ opened }: { opened: boolean }) => {
             },
           }}
         >
-          <Flex direction={"row"} align={"center"} justify={"space-between"}>
-            <UserCircle size={"1.8rem"} />
-            <Box>
-              <Text fw="600" fz="sm">
-                {" "}
-                User Name
-              </Text>
-              <Text
-                fw={600}
-                fz="xs"
-                c={theme.colorScheme === "dark" ? "gray.6" : "gray.7"}
-              >
-                User Email
-              </Text>
-            </Box>
-            <ArrowRight />
-          </Flex>
+          {isLoading ? (
+            <Center>
+              <Loader />
+            </Center>
+          ) : (
+            <Flex direction={"row"} align={"center"} justify={"space-between"}>
+              <UserCircle size={"1.8rem"} />
+              <Box>
+                <Text fw="600" fz="sm">
+                  {data?.data?.data?.firstName
+                    ? data?.data?.data?.firstName
+                    : "User Name"}
+                </Text>
+                <Text
+                  fw={600}
+                  fz="xs"
+                  c={theme.colorScheme === "dark" ? "gray.6" : "gray.7"}
+                >
+                  {data?.data?.data.email}
+                </Text>
+              </Box>
+              <ArrowRight />
+            </Flex>
+          )}
         </UnstyledButton>
       </Navbar.Section>
     </Navbar>
