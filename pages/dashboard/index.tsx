2f118866f54b6aea6cb30bff5499babc568312f8
farhad <rb0214192@gmail.com>
2023-06-26 22:39:02 +0330

    add ssr

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,5 +1,24 @@
+import verifyToken from "@/utils/verifyToken";
 import { Box } from "@mantine/core";
 
 export default function Dashboard() {
   return <Box>home page</Box>;
+}
+
+export async function getServerSideProps(context: any) {
+  const { token } = context.req.cookies;
+  const secretKey = process.env.SECRET_KEY;
+
+  const { email } = await verifyToken(token, secretKey!);
+  if (!email) {
+    return {
+      redirect: { destination: "/signin", permanent: false },
+    };
+  } else {
+    return {
+      props: {
+        email,
+      },
+    };
+  }
 }
