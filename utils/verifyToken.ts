35a23aecb0a21d85ae2adc87d45f652a406affa9
farhad <rb0214192@gmail.com>
2023-06-26 22:38:51 +0330

    fix bug

--- utils/verifyToken.ts
+++ utils/verifyToken.ts
@@ -1,6 +1,9 @@
 import { verify } from "jsonwebtoken";
 
 export default function verifyToken(token: string, secretKey: string) {
+  if (!token) {
+    return false;
+  }
   const result = verify(token!, secretKey!) as any;
   return result;
 }
