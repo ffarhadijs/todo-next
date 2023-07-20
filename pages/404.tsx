90a26eb6ddd429d06e15394737f1bc1cb8dbe008
farhad farhadi <rb0214192@gmail.com>
2023-07-20 20:09:12 +0430

    custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,26 @@
-import { useRouter } from "next/router";
+import Layout from "@/components/layout/Layout";
+import { Center, Text } from "@mantine/core";
 import React from "react";
 
 export default function Custom404() {
 
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <Layout>
+      <Center
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text fz={60} fw={700}>
+          404
+        </Text>
+        <Text fz={32} fw={600}>
+          Page Not Found!
+        </Text>
+      </Center>
+    </Layout>
+  );
 }
-
